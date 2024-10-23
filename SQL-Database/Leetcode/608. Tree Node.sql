e7e73c18b27195a1f24716e00e3262392832719f
moath mahmoud essa haimour <114088314+MOATHHAIMOUR@users.noreply.github.com>
2024-10-24 00:54:14 +0300

    Update 608. Tree Node.sql

--- SQL-Database/Leetcode/608. Tree Node.sql
+++ SQL-Database/Leetcode/608. Tree Node.sql
@@ -5,3 +5,15 @@ when EXISTS((select * from tree T2 where T1.id =  T2.p_id)) then 'Inner'
 else 'Leaf'
 end as type
 from tree T1
+
+
+
+--OR 
+
+Select T1.p_id, 
+case 
+when T1.p_id is null then 'Root'
+when EXISTS((select * from tree T2 where T1.id in (T2.p_id))) then 'Inner'
+else 'Leafe'
+end as nodeType
+from tree T1
